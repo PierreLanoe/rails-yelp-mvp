ca50a49a653dbf9907ea52b775c84176c4fd5b06
Pierre Lanoe <pierre.lanoe@edhec.com>
2018-10-17 16:29:20 +0200

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Chinese East',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0123456789',
+    category:        'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '0987654321',
+    category:        'italian'
+  },
+  {
+    name:         'Jap East',
+    address:      '56A Shoreditch High St, Berlin E1 6PQ',
+    phone_number:  '0987654321',
+    category:        'japanese'
+  },
+  {
+    name:         'Feench East',
+    address:      '56A Shoreditch High St, Paris E1 6PQ',
+    phone_number:  '0987654321',
+    category:        'french'
+  },
+  {
+    name:         'Belgian East',
+    address:      '56A Shoreditch High St, Bruxelles E1 6PQ',
+    phone_number:  '0987654321',
+    category:        'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
